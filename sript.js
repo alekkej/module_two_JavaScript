95574caf5d83cd1192a3a2ef070fcad17f6d00f9
alekkej <aleksiyskok@gmail.com>
2023-02-04 16:17:49 +0600

    homework-01

--- sript.js
+++ sript.js
@@ -2,39 +2,24 @@
 
 let money = 100000;
 let profit = 'Фриланс';
-let expenses = 'Питание, проезд';
-let purpose = 500000;
-let period = 6;
+let expenses = 'Питание, Проезд, ЖКХ';
+let purpose = 60000;
+let period = 11;
 
-console.log('Месячный бюджет', money, 'рублей', typeof money);
-console.log(profit, typeof profit);
-console.log('Длина переменной ежемесячных расходов ' + expenses.length + ' кодовых значений', typeof expenses);
-console.log('Период равен', period, 'месяцев');
-console.log('Цель заработать', purpose, 'рублей');
+console.log('Месячный бюджет ' + typeof money + ' Дополнительный доход ' + typeof profit);
 
-let budgetDay = money / 30;
-console.log('Дневной бюджет', budgetDay, 'рублей');
+console.log('Длина строки ' + expenses.length + ' символов');
 
-console.log('Дополнительное задание:');
+console.log(`Период равен ${period}-ти месяцам`);
 
-function splitExpenses(expenses, separator) {
-    let arrayOfExpenses = expenses.split(separator);
-    console.log('Массив содержит ' + arrayOfExpenses.length + ' элемента/ов');
-}
+console.log(`Цель: заработать ${purpose} рублей`);
 
-let space = ' ';
-let massiveExpenses = expenses.split(/[,]/);
+let budgetOfDay = money / 30;
 
-splitExpenses(expenses, space);
-console.log(expenses .toLowerCase());
-console.log(massiveExpenses);
+console.log(Math.floor(budgetOfDay) + ' рублей/день');
 
+console.log('Дополнительное задание');
 
+console.log(expenses.toLowerCase());
 
-
-
-
-
-
-
-
+console.log(expenses.split(','));
