52c523d4c5d4d649b31cd94e99e393abe99e6e7c
alekkej <aleksiyskok@gmail.com>
2023-02-09 21:33:56 +0600

    homework-02

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 'use strict'
 
-let test = 10;
-console.log(test);
+let numberOne = parseFloat(prompt('Введите первое число'));
+console.log('Первая переменная: ' + (numberOne));
+
+let numberTwo = parseFloat(prompt('Введите второе число')) ;
+console.log('Вторая переменная: ' + (numberTwo));
+
+// console.log(`${20 + 10}`);
+// console.log(`${20 - 10}`);
+// console.log(`${20 / 10}`);
+// console.log(`${20 * 10}`);
+// console.log(`${20 % 10}`);
+
+
+console.log('Математические действия с введенными переменными:');
+
+console.log(`${'+ : ' + (numberOne + numberTwo).toFixed(1)}`);
+console.log(`${'- : ' + (numberOne - numberTwo).toFixed(1)}`);
+console.log(`${'/ : ' + (numberOne / numberTwo).toFixed(1)}`);
+console.log(`${'* : ' + (numberOne * numberTwo).toFixed(1)}`);
+console.log(`${'% : ' + (numberOne % numberTwo).toFixed(1)}`);
 
