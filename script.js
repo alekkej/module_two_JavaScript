ffe25af3ff02c5e3c148846bf1ffec0d287d0b28
Aleksey <120790014+alekkej@users.noreply.github.com>
2023-02-09 22:02:50 +0600

    homework-02

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let numberOne = parseFloat(prompt('Введите первое число'));
 console.log('Первая переменная: ' + (numberOne));
 
-let numberTwo = parseFloat(prompt('Введите второе число')) ;
+let numberTwo = parseFloat(prompt('Введите второе число'));
 console.log('Вторая переменная: ' + (numberTwo));
 
 // console.log(`${20 + 10}`);
