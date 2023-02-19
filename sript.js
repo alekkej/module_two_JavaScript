8a2328fa8a4ab3f2efaea4da5177948465767440
alekkej <aleksiyskok@gmail.com>
2023-02-19 20:46:40 +0600

    homework-03

--- sript.js
+++ sript.js
@@ -1,40 +1,41 @@
 'use strict'
 
-let money = 100000;
-let profit = 'Фриланс';
-let expenses = 'Питание, проезд';
-let purpose = 500000;
-let period = 6;
+let salary = Number(prompt('Ваш месячный доход?'));
+console.log('Месячный доход ' + salary + ' руб.');
 
-console.log('Месячный бюджет', money, 'рублей', typeof money);
-console.log(profit, typeof profit);
-console.log('Длина переменной ежемесячных расходов ' + expenses.length + ' кодовых значений', typeof expenses);
-console.log('Период равен', period, 'месяцев');
-console.log('Цель заработать', purpose, 'рублей');
 
-let budgetDay = money / 30;
-console.log('Дневной бюджет', budgetDay, 'рублей');
+let expenses = prompt('Перечислите возможные расходы за период через запятую');
+console.log('Категории расходов ' + expenses);
 
-console.log('Дополнительное задание:');
+let requiredExpenses = Number(prompt('Во сколько обойдутся обязательные статьи расходов?'));
+console.log('Обязательные расходы ' + requiredExpenses + ' руб.');
 
-function splitExpenses(expenses, separator) {
-    let arrayOfExpenses = expenses.split(separator);
-    console.log('Массив содержит ' + arrayOfExpenses.length + ' элемента/ов');
+
+let hasDeposit = confirm('Есть ли у Вас вклад в банке?');
+console.log(`${hasDeposit ? 'Есть вклад' : 'Вклада нет'}`);
+
+let budgetPerMonth = salary - requiredExpenses;
+console.log('Бюджет на месяц ' + budgetPerMonth + ' руб.');
+
+let budgetPerDay = Math.floor(budgetPerMonth / 30);
+console.log('Бюджет на день ' + budgetPerDay + ' руб.');
+
+let purpose = Number(prompt('Сколько нужно накопить?'));
+console.log('Нужно накопить ' + purpose + ' руб.');
+let months = Math.ceil(purpose / budgetPerMonth);
+
+if (budgetPerMonth > 0) {
+    console.log('Вы придете к цели через ' + months + ' месяца/-ев');
+} else {
+    console.log('Нет бюджета для накоплений');
 }
 
-let space = ' ';
-let massiveExpenses = expenses.split(/[,]/);
-
-splitExpenses(expenses, space);
-console.log(expenses .toLowerCase());
-console.log(massiveExpenses);
-
-
-
-
-
-
-
-
-
-
+if (budgetPerDay < 0) {
+    console.log('Что-то пошло не так');
+} else if (budgetPerDay > 6000) {
+    console.log('У Вас высокий уровень дохода');
+} else if (budgetPerDay < 3000) {
+    console.log('У Вас уровень дохода ниже среднего');
+} else {
+    console.log('У Вас средний уровень дохода');
+}
