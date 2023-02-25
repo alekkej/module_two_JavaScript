47e8fb8fdd7a26fb876c784aa0c246942ea91e46
alekkej <aleksiyskok@gmail.com>
2023-02-25 19:11:58 +0600

    homework-04

--- script.js
+++ script.js
@@ -1,14 +1,89 @@
 'use strict'
 
-let date = new Date();
-let weekday = date.getDay();
+const days = [
+    'Воскреснье',
+    'Понедельник',
+    'Вторник',
+    'Среда',
+    'Четверг',
+    'Пятница',
+    'Суббота'    
+]
 
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const currentDayIndex = new Date().getDay();
+const currentDay = days[currentDayIndex];
+
+// console.log(Date());
+// console.log(currentDayIndex);
+
+console.log(`Текущий день: ${currentDay}`);
+
+function printDay(index) {
+    document.writeIn(days[index])
+}
+
+for (let i = 0; i < days.length; i++) {
+    printDay(i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let date = new Date();
+// let weekday = date.getDay();
+
+// let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+// let lineBreak = [];
+
+// for (let j = 5; j < 7; j++) {
+//     week[j] = `<b>${week[j]}</b>`
+// };
+
+
+// for (let i = 0; i < week.length; i++) {
+//     lineBreak = lineBreak + week[i] + '<br \/>'
+// };
+
+// console.log(lineBreak);
+// document.write(lineBreak);
+// document.write(date);
+// document.write(' ' + weekday);
 
 // week[5] = `<b>${week[5]}</b>`;
 // week[6] = `<b>${week[6]}</b>`;
-
-let lineBreak = [];
 
 // if (weekday = 0) {
 //     week[6] = `<i>${week[6]}</i>`;
@@ -31,22 +106,7 @@ let lineBreak = [];
 //     week[5] = `<i>${week[5]}</i>`;
 // } 
 
-
-for (let j = 5; j < 7; j++) {
-    week[j] = `<b>${week[j]}</b>`
-};
-
 // for (let d = 0; d < week.length; d++) {
 //     week[d] = `<i>${week[d]}</i>`;
 // }
 
-
-for (let i = 0; i < week.length; i++) {
-    lineBreak = lineBreak + week[i] + '<br \/>'
-};
-
-
-console.log(lineBreak);
-document.write(lineBreak);
-document.write(date);
-document.write(' ' + weekday);
